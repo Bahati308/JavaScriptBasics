88ee78180b573ed0d5a0df825bd6135421f824b6
@Bahati308 <bahatibrianp@gmail.com>
2023-02-02 18:08:36 +0300

    added index2.js and modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // this is my first js program ever
 // developed 0n Thur Feb 02, 23
 
-console.log('hello world');
+console.log('hello world')
---
+++ index2.js
@@ -0,0 +1,6 @@
+// second program.
+
+const place = "world"
+const greeting = "hello"
+
+console.log("%s, %s,", greeting, place)
