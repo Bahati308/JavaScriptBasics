df6cbb96ece966e0c769b0904b79ed530bfda713
@Bahati308 <bahatibrianp@gmail.com>
2023-02-07 17:31:21 +0300

    added new files

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+//
+
+const people = ["Brian", "Apple", "Jack"];
+const one =1;
+const str = "Hello World";
+const b = true;
+const person ={
+    firstName: "brian",
+    lastName: "br"
+}
+
+function sayhello(person){
+    console.log("hello " + person.firstName);
+}
+
+console.log("******type of******");
+console.log(typeof people);
+console.log(typeof one);
+console.log(typeof str)
+console.log(typeof b)
+console.log(typeof person)
+console.log(typeof sayhello)
