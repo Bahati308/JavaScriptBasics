9852e8989a0f025d68207c6ee18d7d11300ebd9b
@Bahati308 <bahatibrianp@gmail.com>
2023-02-07 17:16:21 +0300

    added new files

--- conc.js
+++ conc.js
@@ -4,10 +4,10 @@
 
 // using the + operator
 
-let str1 = "hello";
+let str1 = "hello ";
 let str2 = 'World';
 
 console.log(str1 + str2);
-console.log(str1 + " Big " + str2);
+console.log(str1 + "Big " + str2);
 
 
