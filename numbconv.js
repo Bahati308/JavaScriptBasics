a3c3e7a4aa5bdf52956514e588f46f0923e9dabd
@Bahati308 <bahatibrianp@gmail.com>
2023-02-08 17:45:12 +0300

    added new js files

---
+++ numbconv.js
@@ -0,0 +1,9 @@
+// this programdemonstrates the conversion of 
+// numbers to strings and the reverse
+
+let num1 = '150';
+
+console.log(parseFloat('100'));
+console.log(parseInt(num1));
+console.log(parseInt('ABC'));
+console.log(parseInt('0xF'));// treated as a hexadecimal 
