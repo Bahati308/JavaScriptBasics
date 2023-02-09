6376b3eb92e909ef61350a107dad23dd1f320d82
@Bahati308 <bahatibrianp@gmail.com>
2023-02-09 17:39:07 +0300

    added errorHandling.js

---
+++ errorHandling.js
@@ -0,0 +1,9 @@
+// this program deals with errors 
+// erros can not miss in the code, 
+// So we need to learn how to handle them
+
+throw 'myException';
+    throw true;
+
+
+// we catch exceptions by using try, catch and finally
