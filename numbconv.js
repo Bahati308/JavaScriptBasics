43033d191b35d2bfe2e15c73ee601b0e63f0a239
@Bahati308 <bahatibrianp@gmail.com>
2023-02-09 17:27:28 +0300

    modified numbconv.js

--- numbconv.js
+++ numbconv.js
@@ -7,3 +7,8 @@ console.log(parseFloat('100'));
 console.log(parseInt(num1));
 console.log(parseInt('ABC'));
 console.log(parseInt('0xF'));// treated as a hexadecimal 
+
+let flo1 = '1.50';
+console.log (parseFloat('1.00'));
+console.log(parseFloat(flo1));
+console.log (parseFloat('ABC'));
