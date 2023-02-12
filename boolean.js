36177421c52881a29998ee8c79cb33e13f80309e
@Bahati308 <bahatibrianp@gmail.com>
2023-02-12 16:02:39 +0300

    added boolean.js

--- boolean.js
+++ boolean.js
@@ -1,3 +1,12 @@
 // sun feb 12, 23
 // this is about the boolean logic
 
+const status = 400;
+
+if (status === 200){
+    //true side 
+    console.log('OK!');}
+    else if(status===400){
+        console.log('Error');}
+else {
+    console.log('Unknown stat   us');} 
