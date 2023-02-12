b656f3f1366bc362a1d14d56d36f8811f18cb185
@Bahati308 <bahatibrianp@gmail.com>
2023-02-12 15:08:37 +0300

    modified dates.js

--- dates.js
+++ dates.js
@@ -6,4 +6,23 @@
 
 const now = new Date(); 
 
+console.log(now);// this prints out the default time
+
+//however, we can customize the time we want.
+
+const win9launch = new Date(1997, 2, 10);// we specified the date we want 
+
+console.log(win9launch);
+
+//we can set have more customizations too
+
+const demoDate = new Date();
+
+demoDate.setMonth(0);
+console.log(demoDate);
+
+// we can get the dates in the following formats too
+
+console.log(`Day of week : ${demoDate.getDay()}`)
+
 
