88fcdd22e3930399c53a98a50f01f976d0f15393
@Bahati308 <bahatibrianp@gmail.com>
2023-02-12 16:07:38 +0300

    added boolean_1.js

---
+++ boolean_1.js
@@ -0,0 +1,9 @@
+// sun feb 12, 23
+// this is about the boolean logic
+//we have removed the brackets 
+//since it is all single line code  
+const status = 200;
+
+if (status === 200) console.log('OK!');
+    else if(status===400)console.log('Error');
+else console.log('Unknown stat   us'); 
