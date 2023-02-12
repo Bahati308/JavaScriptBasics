d87b815c9f117914e0dd37c6d33c86e4663f15df
@Bahati308 <bahatibrianp@gmail.com>
2023-02-12 16:18:47 +0300

    modified  ternary.js

--- ternary.js
+++ ternary.js
@@ -2,14 +2,18 @@
 
 const status =200;
 
-let message = '';
+// let message = '';
 
-if (status === 200){
-    message = 'OK !'
-}
+// if (status === 200){
+//     message = 'OK !'
+// }
 
-    else{
-        message = 'Error !'
-    }
+//     else{
+//         message = 'Error !'
+//     }
 
+
+// this code gives the same results as the one commented out
+
+const message = (status === 200)? 'OK !' : 'Error !';
     console.log(message);
