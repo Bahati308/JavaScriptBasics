fda71af5e5e47f1370242cbad9386ce6b4322135
@Bahati308 <bahatibrianp@gmail.com>
2023-02-12 16:13:10 +0300

    added ternary.js

---
+++ ternary.js
@@ -0,0 +1,15 @@
+// sun feb 12, 23
+
+const status =400;
+
+let message = '';
+
+if (status === 200){
+    message = 'OK !'
+}
+
+    else{
+        message = 'Error !'
+    }
+
+    console.log(message);
