5941e7668beaf87cb8a057236a16aaacb83caee2
@Bahati308 <bahatibrianp@gmail.com>
2023-02-13 17:33:21 +0300

    modified arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,14 @@
 // an array is a collection of
 // different values and can be of different data types
 
+// we examine 2 ways to create arrays 
+
+let arrylength = 5;
+
+let arr1 = [];// first method
+let arr2 = Array(arrylength); //2nd method
+
+// let arr2 = Array(5); this is also possible
+
+console.log(arr1.length);
+console.log(arr2.length);
