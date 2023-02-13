90b1f31599baf7d23716d2566e8abfe5f72bcc1b
@Bahati308 <bahatibrianp@gmail.com>
2023-02-13 17:09:26 +0300

    added and.js

---
+++ and.js
@@ -0,0 +1,11 @@
+const stat = 200;
+
+    if (stat === 200){
+        console.log('OK!');
+    }
+     else if (stat === 400 || stat ===500){
+                console.log('Error !');
+            }
+        else {
+            console.log('unknown status');
+        }
