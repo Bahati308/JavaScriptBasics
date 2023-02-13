6647cab6572846467eb8b188e43ec38db5b61820
@Bahati308 <bahatibrianp@gmail.com>
2023-02-13 17:00:02 +0300

    added case_sensitive.js

---
+++ case_sensitive.js
@@ -0,0 +1,12 @@
+// we are trying to compare the variables
+// and the messages too in this case the case-sensitiveness 
+
+const stat = 'error';
+
+if (stat === 'ERROR !') {
+    console.log ('oops, something went wrong !')
+}
+    else {
+        console.log('looks dope');
+
+    }
