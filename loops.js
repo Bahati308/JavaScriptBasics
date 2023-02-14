540d1ff7f4b260942b10f6564c130341251bac75
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 16:04:39 +0300

    modified loops.js

---
+++ loops.js
@@ -0,0 +1,9 @@
+// this is about loops or iteration
+
+//while, for ,and for ......of are the most common
+
+const names = ['jane', 'jackie', 'christine'];
+
+console.log('----while loop-----');
+
+let 
