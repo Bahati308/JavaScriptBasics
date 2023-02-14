695cdf9336e9088652d48da75437aa5931811451
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 15:05:36 +0300

    modified shiftnunshift.js

--- shiftnunshift.js
+++ shiftnunshift.js
@@ -1,2 +1,13 @@
 //this is about shift and unshift
 
+let arr1 = ['hello', 1, true];
+
+console.log(arr1);
+
+console.log (arr1.unshift('new value'));
+
+console.log(arr1);
+
+console.log(arr1.shift()); //
+
+console.log(arr1);
