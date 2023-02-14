7cd4312630cb000833e0a889c430cfc2ed692114
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 14:59:37 +0300

    modified new js

--- arraymthds.js
+++ arraymthds.js
@@ -4,8 +4,13 @@
 
 let arr1 = ['A', true, 9];
 
-console.log (arr1.push('new value'));
-console.log(arr1);
-console.log(arr1.pop()); // remove last value
+console.log (arr1.push('new value'));//adds one more value
+// and returns its length
+
+console.log(arr1);// now this will have new values from
+// those of the original
+
+console.log(arr1.pop()); //removes value from end of the 
+// array and returns removed vlue
 console.log(arr1);
 
