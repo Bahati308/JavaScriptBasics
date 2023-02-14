3ef5b0fa804c95aaf78fc789fa7e0f9286613f99
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 15:10:05 +0300

    modified shiftnunshift.js

--- shiftnunshift.js
+++ shiftnunshift.js
@@ -2,12 +2,15 @@
 
 let arr1 = ['hello', 1, true];
 
-console.log(arr1);
+console.log(arr1);// original content
 
-console.log (arr1.unshift('new value'));
+console.log (arr1.unshift('new value'));//unshift adds 
+// a value at the beginning of the array and returns the 
+// new array length
 
-console.log(arr1);
+console.log(arr1);// after a new value is added
 
-console.log(arr1.shift()); //
+console.log(arr1.shift()); //removes and prints the 
+// previously added value
 
 console.log(arr1);
