39582e1e27fa6dc2b8eada1120b990369a2ae932
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 14:42:19 +0300

    added new js

---
+++ more_on_arrays.js
@@ -0,0 +1,7 @@
+// let us go deeper in arrays 
+
+let arr1 = ["A", true, 5];
+console.log(arr1.length);
+console.log(arr1[3]); // we know this does not exist
+console.log(arr1[2]); // last index
+console.log (arr1[arr1.length -1])// 2nd index
