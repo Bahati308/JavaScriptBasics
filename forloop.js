b2bbae0a820d1bdbb3d1c0d3373b2c89ff65f7f2
@Bahati308 <bahatibrianp@gmail.com>
2023-02-14 16:16:36 +0300

    added forloop.js

--- forloop.js
+++ forloop.js
@@ -3,3 +3,7 @@
 const names = ['baptist', 'eric', 'douglas'];
 
 console.log('---for loop----');
+for (let forindex = 0; forindex < names.length; forindex++) {
+    const element = names[forindex];
+    console.log(element);
+}
