5c4aa6aa499dd5ef6a8c46d234cbb1abeb3ec455
@Bahati308 <bahatibrianp@gmail.com>
2023-02-15 16:58:00 +0300

    added content in functions.js

--- functions.js
+++ functions.js
@@ -1,2 +1,8 @@
 // a function is just a block of executible code
+// they are called by simply using the 'function' key word
 
+function printHello(params) {
+    
+}
+
+console.log(typeof printHello);
