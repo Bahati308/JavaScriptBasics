8cbaf4fad39a60690db5e2c3e847ed06a221e13c
@Bahati308 <bahatibrianp@gmail.com>
2023-02-16 16:49:21 +0300

    added content in fatarrFunc.js

--- fatarrFunc.js
+++ fatarrFunc.js
@@ -5,4 +5,6 @@
 const add = (a,b) => a+b;
     console.log(add(1,2)); // being a single line, we can leave out the 'return'
 
-    
+ const subtract =(a,b) =>{
+    return a-b
+ };   console.log(subtract(2,1));   
