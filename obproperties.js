103d87f2c4651b9ee3f66282a716b4a92202e761
@Bahati308 <bahatibrianp@gmail.com>
2023-02-16 18:07:54 +0300

    added content  obproperties.js

--- obproperties.js
+++ obproperties.js
@@ -5,3 +5,17 @@
 //1. dot notation(<object>.name)
 //2. bracket notation(<object>["name"])
 
+
+const newbook = new Object();
+newbook.title ="2023";
+newbook.author = "abc def";
+newbook.isAvailable =true;
+
+newbook.checkIn = function(){
+    this.isAvailable = true;
+
+}
+
+newbook.chekOut =function(){
+    this.isAvailable = false;//note the use of the 'this' key word
+}
