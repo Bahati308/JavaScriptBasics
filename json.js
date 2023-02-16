0df8ff1dec351f32736337f1aa77e93f49378e48
@Bahati308 <bahatibrianp@gmail.com>
2023-02-16 17:33:23 +0300

    added content to json.js

--- json.js
+++ json.js
@@ -2,6 +2,9 @@
 //1. json.stingify and 2. json.parse
 //btw, json= js object notation
 
+
+//objects to strings
+
 // Object format
 
 const book= {
@@ -13,3 +16,13 @@ const book= {
 //converted to JSON
 const bookJSON =JSON.stringify(book);
 console.log(bookJSON);
+
+//strings to objects(restoring the original objects)
+
+//json text
+
+var jsonObj = '{"title ":"going","author":"Def Ghi","isAvailable","isAvailable":true}';
+
+//to object
+var book1 =JSON.parse(jsonObj);
+console.log(book.title);
