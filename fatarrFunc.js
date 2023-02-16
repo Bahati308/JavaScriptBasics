5de60f690115342cf99e26d7182390b6b93d1af1
@Bahati308 <bahatibrianp@gmail.com>
2023-02-16 16:46:51 +0300

    added content in fatarrFunc.js

--- fatarrFunc.js
+++ fatarrFunc.js
@@ -1,3 +1,8 @@
 // => we use this symbol
 // fat array functions 
 //aka array functions
+
+const add = (a,b) => a+b;
+    console.log(add(1,2)); // being a single line, we can leave out the 'return'
+
+    
