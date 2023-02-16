db3f5bb48644b7a7c33f6b942f86add7a65b0f4d
@Bahati308 <bahatibrianp@gmail.com>
2023-02-16 17:22:32 +0300

    added content to json.js

--- json.js
+++ json.js
@@ -2,3 +2,14 @@
 //1. json.stingify and 2. json.parse
 //btw, json= js object notation
 
+// Object format
+
+const book= {
+    title: "1990",
+    author: "Abc Def",
+    isAvailable: false
+};
+
+//converted to JSON
+const bookJSON =JSON.stringify(book);
+console.log(bookJSON);
