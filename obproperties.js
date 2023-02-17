f34b835adb24adedcb5c0a97adb5b636f48288e7
@Bahati308 <bahatibrianp@gmail.com>
2023-02-17 19:25:25 +0300

    added content  obproperties.js

--- obproperties.js
+++ obproperties.js
@@ -5,7 +5,7 @@
 //1. dot notation(<object>.name)
 //2. bracket notation(<object>["name"])
 
-
+//here we are using consructors to create objects
 const newbook = new Object();
 newbook.title ="2023";
 newbook.author = "abc def";
