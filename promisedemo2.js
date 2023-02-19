8fa04e6fa641cd330a78fee38cf587f8f1dfa7ad
@Bahati308 <bahatibrianp@gmail.com>
2023-02-19 15:11:25 +0300

    modified  promisedemo2.js

--- promisedemo2.js
+++ promisedemo2.js
@@ -2,23 +2,28 @@
 
 function promiseTimeout(ms) {
     return new Promise ((resolve, reject) => {
-        setTimeout(reject,ms);// this reject thing will return error,
+        setTimeout(resolve,ms);// this reject thing will return error,
                             // and the resolve thing will return the 
                             // intended messages
     });
 }
 
 promiseTimeout(2000)
+
     .then(() => {
-        console.log('DONE after 1s or 1000ms!!')// this will be printed 
+
+        console.log('this is a success because the above parameter was resolve')
+
+        console.log('success, DONE after 1s or 1000ms!!')// this will be printed 
                                 //when all is well
           return promiseTimeout(1000);
     })
     .then(()=>{
-    console.log('DONE TOO, BUT after 2s 0r 2000ms')
+    console.log('success, DONE TOO, BUT after 2s 0r 2000ms')
     })
     
     
     .catch(()=>{
+        console.log('this is an error since the parameter is set to reject')
             console.log('ERROR !!')//in case it went otherwise
     })
