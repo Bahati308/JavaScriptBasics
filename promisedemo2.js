d627ded92c28f60647f8d1f981dda3761776861b
@Bahati308 <bahatibrianp@gmail.com>
2023-02-19 15:04:57 +0300

    added  promisedemo2.js

---
+++ promisedemo2.js
@@ -0,0 +1,22 @@
+// lets look at some examples
+
+function promiseTimeout(ms) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(reject,ms);
+    });
+}
+
+promiseTimeout(2000)
+    .then(() => {
+        console.log('DONE after 1s or 1000ms!!')// this will be printed 
+                                //when all is well
+          return promiseTimeout(1000);
+    })
+    .then(()=>{
+    console.log('DONE TOO, BUT after 2s 0r 2000ms')
+    })
+    
+    
+    .catch(()=>{
+            console.log('ERROR !!')//in case it went otherwise
+    })
